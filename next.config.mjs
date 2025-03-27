671673a3a6831195803a4d64618405de7ea565f9
Talha Yüce <yucetalha8290@gmail.com>
2025-03-27 13:34:18 +0300

    v.1

--- next.config.mjs
+++ next.config.mjs
@@ -69,14 +69,6 @@ const nextConfig = {
       },
     ];
   },
-  async rewrites() {
-    return [
-      {
-        source: "/sitemap.xml",
-        destination: "/sitemap.xml",
-      },
-    ];
-  },
 };
 
 mergeConfig(nextConfig, userConfig);
