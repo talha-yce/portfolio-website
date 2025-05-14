0d8304d4a66f19cb762d056e8b5967b734687b2f
Talha Yüce <yucetalha8290@gmail.com>
2025-05-14 14:25:19 +0300

    Update Next.js configuration to allow images from the new domain 'talha-yuce.site' for enhanced asset management and flexibility in image sourcing.

--- next.config.mjs
+++ next.config.mjs
@@ -17,7 +17,7 @@ const nextConfig = {
     dangerouslyAllowSVG: true,
     contentDispositionType: 'attachment',
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    domains: ["images.unsplash.com"],
+    domains: ["images.unsplash.com", "talha-yuce.site"],
     formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
